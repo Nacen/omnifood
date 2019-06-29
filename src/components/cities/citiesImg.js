682b522d6010d366095967a198b29e647417a017
Vincent <overlordnacen@gmail.com>
2019-06-29 18:31:08 +0800

    Update Cities Section

--- src/components/cities/citiesImg.js
+++ src/components/cities/citiesImg.js
@@ -3,9 +3,11 @@ import london from "../../img/london.jpg";
 import sanFrancisco from "../../img/san-francisco.jpg";
 import berlin from "../../img/berlin.jpg";
 
-export const cities = {
-    lisbon,
-    london,
-    sanFrancisco,
-    berlin
+const cities = {
+  lisbon,
+  london,
+  sanFrancisco,
+  berlin
 };
+
+export default cities;
