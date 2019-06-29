39c83c46b02d6202a2758b9de5d1e303b75a6013
Vincent <overlordnacen@gmail.com>
2019-06-29 18:31:40 +0800

    Update Features Sections Styling

--- src/components/features/features.js
+++ src/components/features/features.js
@@ -18,9 +18,9 @@ const Features = () => {
           </Col>
         </Row>
 
-        <Row>
+        <Row className="features">
           <Col className="box" md={3} s={6} xs={6}>
-            <img src={infinite.ios} className="icon" alt="infinity icon" />
+            <img src={infinite.icon} className="icon" alt="infinity icon" />
             <h3>Up to 365 days/year</h3>
             <p>
               Never cook again! We really mean that. Our subscription plans
@@ -29,7 +29,7 @@ const Features = () => {
             </p>
           </Col>
           <Col className="box" md={3} s={6} xs={6}>
-            <img src={stopwatch.ios} className="icon" alt="stopwatch icon" />
+            <img src={stopwatch.icon} className="icon" alt="stopwatch icon" />
             <h3>Ready in 20 minutes</h3>
             <p>
               You're only twenty minutes away from your delicious and super
@@ -38,7 +38,7 @@ const Features = () => {
             </p>
           </Col>
           <Col className="box" md={3} s={6} xs={6}>
-            <img src={nutrition.ios} className="icon" alt="nutrition icon" />
+            <img src={nutrition.icon} className="icon" alt="nutrition icon" />
             <h3>100% organic</h3>
             <p>
               All our vegetables are fresh, organic and local. Animals are
@@ -47,7 +47,7 @@ const Features = () => {
             </p>
           </Col>
           <Col className="box" md={3} s={6} xs={6}>
-            <img src={cart.ios} className="icon" alt="cart icon" />
+            <img src={cart.icon} className="icon" alt="cart icon" />
             <h3>Order anything</h3>
             <p>
               We don't limit your creativity, which means you can order whatever
