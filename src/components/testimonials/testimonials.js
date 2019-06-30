6ccda39fa25629df607142cb70222ee510f50c98
Vincent <overlordnacen@gmail.com>
2019-06-30 15:54:47 +0800

    Finish styling testimonials section

--- src/components/testimonials/testimonials.js
+++ src/components/testimonials/testimonials.js
@@ -12,36 +12,36 @@ const Testimonials = () => {
         <h2>Our customers can't live without us</h2>
         <Row>
           <Col xs={12} md={4} lg={4}>
-            <blockquote>
+            <blockquote className="customer-testimonials">
               Omnifood is just awesome! I just launched a startup which leaves
               me with no time for cooking, so Omnifood is a life-saver. Now that
               I got used to it, I couldn't live without my daily meals!
-              <cite>
-                <img src={customerAlberto} alt="Customer Alberto Duncan" />
-                Alberto Duncan
+              <cite className="customer-cite">
+                <img src={customerAlberto} className="customer-img" alt="Customer Alberto" />
+                {"\u2014 "} Alberto Duncan
               </cite>
             </blockquote>
             <div />
           </Col>
           <Col xs={12} md={4} lg={4}>
-            <blockquote>
+            <blockquote className="customer-testimonials">
               Inexpensive, healthy and great-tasting meals, delivered right to
               my home. We have lots of food delivery here in Lisbon, but no one
               comes even close to Omifood. Me and my family are so in love!
-              <cite>
-                <img src={customerJoana} alt="" />
-                Joana Silva
+              <cite className="customer-cite">
+                <img src={customerJoana} className="customer-img" alt="Customer Joana" />
+                {"\u2014 "} Joana Silva
               </cite>
             </blockquote>
           </Col>
           <Col xs={12} md={4} lg={4}>
-            <blockquote>
+            <blockquote className="customer-testimonials">
               I was looking for a quick and easy food delivery service in San
               Franciso. I tried a lot of them and ended up with Omnifood. Best
               food delivery service in the Bay Area. Keep up the great work!
-              <cite>
-                <img src={customerMilton} alt="" />
-                Milton Chapman
+              <cite className="customer-cite">
+                <img src={customerMilton} className="customer-img" alt="Customer Milton" />
+                {"\u2014 "} Milton Chapman
               </cite>
             </blockquote>
           </Col>
