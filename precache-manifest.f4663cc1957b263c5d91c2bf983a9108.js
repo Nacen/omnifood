8692734ac5f1192ef56a23ee0584db16e9c13bee
Vincent <overlordnacen@gmail.com>
2019-07-07 12:59:31 +0800

    Updates

---
+++ precache-manifest.f4663cc1957b263c5d91c2bf983a9108.js
@@ -0,0 +1,154 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f20db02d681ede0762bf96c3a82787",
+    "url": "/omnifood/index.html"
+  },
+  {
+    "revision": "4b465d7ad2024027cab7",
+    "url": "/omnifood/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "82927655c6b2deb94a46",
+    "url": "/omnifood/static/css/main.203af9f0.chunk.css"
+  },
+  {
+    "revision": "4b465d7ad2024027cab7",
+    "url": "/omnifood/static/js/2.0df7db50.chunk.js"
+  },
+  {
+    "revision": "82927655c6b2deb94a46",
+    "url": "/omnifood/static/js/main.02045cee.chunk.js"
+  },
+  {
+    "revision": "596000ce019b1f44383b",
+    "url": "/omnifood/static/js/runtime~main.b506f9ac.js"
+  },
+  {
+    "revision": "ec4871fac5f8b622a6f5243352affdd7",
+    "url": "/omnifood/static/media/1.ec4871fa.jpg"
+  },
+  {
+    "revision": "5d8068eb14cea195d405a361f4eecb66",
+    "url": "/omnifood/static/media/2.5d8068eb.jpg"
+  },
+  {
+    "revision": "b92586dc030ceb44bc304401ac8c4727",
+    "url": "/omnifood/static/media/3.b92586dc.jpg"
+  },
+  {
+    "revision": "d2b48ab94a7b069460230c6a39f083f1",
+    "url": "/omnifood/static/media/4.d2b48ab9.jpg"
+  },
+  {
+    "revision": "3a7dc31cd1986592c850f5c6efa48332",
+    "url": "/omnifood/static/media/5.3a7dc31c.jpg"
+  },
+  {
+    "revision": "db4c17bfcfd8568dd9716e1b264f900e",
+    "url": "/omnifood/static/media/6.db4c17bf.jpg"
+  },
+  {
+    "revision": "0d0e0bf3945510b34f1c83a652eee2a8",
+    "url": "/omnifood/static/media/7.0d0e0bf3.jpg"
+  },
+  {
+    "revision": "30d387f17b860ae8c8afc5b8b5f91d4d",
+    "url": "/omnifood/static/media/8.30d387f1.jpg"
+  },
+  {
+    "revision": "2bf4f65ea58f7723da714750d68fd040",
+    "url": "/omnifood/static/media/app-iPhone.2bf4f65e.png"
+  },
+  {
+    "revision": "a17c3da3b81543f4e30bec3166fc6517",
+    "url": "/omnifood/static/media/back-customers.a17c3da3.jpg"
+  },
+  {
+    "revision": "a3ec74fd5cc015af1da7118b130c5ffa",
+    "url": "/omnifood/static/media/berlin.a3ec74fd.jpg"
+  },
+  {
+    "revision": "b56b435116658f4521f1a9a16d482501",
+    "url": "/omnifood/static/media/customer-1.b56b4351.jpg"
+  },
+  {
+    "revision": "9b6c894aa65ca6695c00f356c02555bb",
+    "url": "/omnifood/static/media/customer-2.9b6c894a.jpg"
+  },
+  {
+    "revision": "d0558d91063038236b60e3ef71fdc1fd",
+    "url": "/omnifood/static/media/download-app.d0558d91.svg"
+  },
+  {
+    "revision": "6923d550a41d50c026d7e765afd78af5",
+    "url": "/omnifood/static/media/hero.6923d550.jpg"
+  },
+  {
+    "revision": "6dafdcf7b4d7a391b89df6ce10ca6035",
+    "url": "/omnifood/static/media/ios-cart.6dafdcf7.svg"
+  },
+  {
+    "revision": "a9500f2ca652ced3383fdcd7d43244ab",
+    "url": "/omnifood/static/media/ios-checkmark.a9500f2c.svg"
+  },
+  {
+    "revision": "00fc7684af755f8b70dc5bd574261219",
+    "url": "/omnifood/static/media/ios-infinite.00fc7684.svg"
+  },
+  {
+    "revision": "66045a0615d50c2c0bb2916ad47a2e6b",
+    "url": "/omnifood/static/media/ios-nutrition.66045a06.svg"
+  },
+  {
+    "revision": "e3be090c5560099003a3d553e7af3773",
+    "url": "/omnifood/static/media/ios-person.e3be090c.svg"
+  },
+  {
+    "revision": "63ccbdb004be963e91d092cf409d77eb",
+    "url": "/omnifood/static/media/ios-star.63ccbdb0.svg"
+  },
+  {
+    "revision": "0363235abbfeec02080392a0a91a28f7",
+    "url": "/omnifood/static/media/ios-stopwatch.0363235a.svg"
+  },
+  {
+    "revision": "0fb4a5fedca2d2524d41b03a37c8d74e",
+    "url": "/omnifood/static/media/lisbon.0fb4a5fe.jpg"
+  },
+  {
+    "revision": "46c0bd59adb9b6ca394698fd6169b290",
+    "url": "/omnifood/static/media/logo-facebook.46c0bd59.svg"
+  },
+  {
+    "revision": "4af031c17f7b7b42a97a16ec770533d2",
+    "url": "/omnifood/static/media/logo-google.4af031c1.svg"
+  },
+  {
+    "revision": "c1dbcbd5a81080b7bc53b98bac4d58bd",
+    "url": "/omnifood/static/media/logo-instagram.c1dbcbd5.svg"
+  },
+  {
+    "revision": "2e982107d221656a8375676a5189e149",
+    "url": "/omnifood/static/media/logo-twitter-orange.2e982107.svg"
+  },
+  {
+    "revision": "3ee3ca382ffca890ed37a4b96d876520",
+    "url": "/omnifood/static/media/logo-twitter.3ee3ca38.svg"
+  },
+  {
+    "revision": "93f9c92588a66b37be5b7ec4bc950858",
+    "url": "/omnifood/static/media/logo-white.93f9c925.png"
+  },
+  {
+    "revision": "6762543a5c1deed3472b676309560f77",
+    "url": "/omnifood/static/media/logo.6762543a.png"
+  },
+  {
+    "revision": "f42da5700bb50339bf6d09a18b820b1d",
+    "url": "/omnifood/static/media/london.f42da570.jpg"
+  },
+  {
+    "revision": "817c89356d01e29c8789fdabf859ae7f",
+    "url": "/omnifood/static/media/san-francisco.817c8935.jpg"
+  }
+]);
